f4b69a2b1aab609458014497fdc38b0d3ef2c5de
Bearon.A <Bearon.A@309-TD02>
2024-10-07 16:21:00 +0100

    Add project files.

---
+++ MergeSort/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var items = new[] { "Florida", "Georgia", "Delaware", "Alabama" };
+
+
+var sorted = Merge(
+    new[] { "Florida", "Georgia" },
+    new[] { "Alabama", "Delaware" });
+
+string[] Merge(string[] list1, string[] list2)
+{
+    var newlist = new string[list1.Length + list2.Length];
+    
+    
+    return newlist;
+}
