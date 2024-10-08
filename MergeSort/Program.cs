83f20ad634c01ea279dbad86a44b039224f32938
ajbearon <99669470+ajbearon@users.noreply.github.com>
2024-10-08 12:52:15 +0100

    Completed MergeSort

--- MergeSort/Program.cs
+++ MergeSort/Program.cs
@@ -1,17 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Diagnostics;
 
-Console.WriteLine("Hello, World!");
-
-var items = new[] { "Florida", "Georgia", "Delaware", "Alabama" };
-
-
+// 1st test that Merge successfully merges 2 lists
 var sorted = Merge(
     new[] { "Florida", "Georgia" },
     new[] { "Alabama", "Delaware" });
 
+// Verify if Merge worked
+Debug.Assert(sorted[0] == "Alabama");
+Debug.Assert(sorted[1] == "Delaware");
+Debug.Assert(sorted[2] == "Florida");
+Debug.Assert(sorted[3] == "Georgia");
 
-Debugger.Break();
+// Now test the full MergeSort
+var items = new[] { "Florida", "Georgia", "Delaware", "Alabama" };
+MergeSort(items);
+Debug.Assert(items[0] == "Alabama");
+Debug.Assert(items[1] == "Delaware");
+Debug.Assert(items[2] == "Florida");
+Debug.Assert(items[3] == "Georgia");
+
+Console.WriteLine("Finished.  All items sorted successfully.");
+
+void MergeSort(string[] items)
+{
+    var sortedLists = new List<string[]>();
+    for (int i = 0; i < items.Length; i++)
+    {
+        sortedLists.Add(new[] { items[i] });
+    }
+
+    while (sortedLists.Count > 1)
+    {
+        int index = 0;
+        while (index < sortedLists.Count - 1)
+        {
+            var sortedList = Merge(sortedLists[index], sortedLists[index + 1]);
+            sortedLists[index] = sortedList;
+            sortedLists.RemoveAt(index + 1);
+            index++;
+        }
+    }
+
+    for (int i = 0; i < items.Length; i++)
+    {
+        items[i] = sortedLists[0][i];
+    }
+}
 
 string[] Merge(string[] list1, string[] list2)
 {
@@ -51,7 +86,7 @@ string[] Merge(string[] list1, string[] list2)
     }
     while (index2 < list2.Length)
     {
-        newlist[indexNewList++] = list1[index2++];
+        newlist[indexNewList++] = list2[index2++];
     }
 
     return newlist;
