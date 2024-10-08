7305868b451b55f338abe739dda62baf6836fcb2
ajbearon <99669470+ajbearon@users.noreply.github.com>
2024-10-08 13:12:20 +0100

    Added TODO items

--- MergeSort/Program.cs
+++ MergeSort/Program.cs
@@ -12,29 +12,48 @@ Debug.Assert(sorted[1] == "Delaware");
 Debug.Assert(sorted[2] == "Florida");
 Debug.Assert(sorted[3] == "Georgia");
 
+
+// Test that Merge successfully merges 2 lists where 1 item is the same
+sorted = Merge(
+    new[] { "Alabama", "Florida" },
+    new[] { "Alabama", "Delaware" });
+
+// Verify if Merge worked
+Debug.Assert(sorted[0] == "Alabama");
+Debug.Assert(sorted[1] == "Alabama");
+Debug.Assert(sorted[2] == "Delaware");
+Debug.Assert(sorted[3] == "Florida");
+
+
+
 // Now test the full MergeSort
-var items = new[] { "Florida", "Georgia", "Delaware", "Alabama" };
+var items = new[] { "Florida", "Georgia", "Delaware", "Alabama", "Washington", "Delaware" };
 MergeSort(items);
 Debug.Assert(items[0] == "Alabama");
 Debug.Assert(items[1] == "Delaware");
-Debug.Assert(items[2] == "Florida");
-Debug.Assert(items[3] == "Georgia");
+Debug.Assert(items[2] == "Delaware");
+Debug.Assert(items[3] == "Florida");
+Debug.Assert(items[4] == "Georgia");
+Debug.Assert(items[5] == "Washington");
 
-Console.WriteLine("Finished.  All items sorted successfully.");
+Console.WriteLine("Finished.");
 
 void MergeSort(string[] items)
 {
+    // TODO - add comment to explain this code
     var sortedLists = new List<string[]>();
     for (int i = 0; i < items.Length; i++)
     {
         sortedLists.Add(new[] { items[i] });
     }
 
+    // TODO - add comment to explain this code
     while (sortedLists.Count > 1)
     {
         int index = 0;
         while (index < sortedLists.Count - 1)
         {
+            // TODO - add comment to explain this code
             var sortedList = Merge(sortedLists[index], sortedLists[index + 1]);
             sortedLists[index] = sortedList;
             sortedLists.RemoveAt(index + 1);
@@ -57,26 +76,29 @@ string[] Merge(string[] list1, string[] list2)
 
     while (index1 < list1.Length && index2 < list2.Length)
     {
+        // TODO - Look at the docs for String.Compare and add a comment here.
         var comparison = String.Compare(list1[index1], list2[index2]);
         if (comparison > 0)
         {
+            // TODO - update this comment to explain this code
             newlist[indexNewList] = list2[index2];
             indexNewList = indexNewList + 1;
             index2++;
         }
         else if (comparison < 0)
         {
-            newlist[indexNewList] = list1[index1];
+            // TODO - fix this code
+            // It will be similar to "comparison > 0" but use list1 instead of list2
+            newlist[indexNewList] = list2[index2];
             indexNewList = indexNewList + 1;
-            index1++;
+            index2++;
         }
         else if (comparison == 0)
         {
-            newlist[indexNewList] = list1[index1];
-            newlist[indexNewList] = list2[index2];
-            indexNewList = indexNewList + 2;
+            // TODO - fix this block
+            // It needs to copy items from both list1 and list2.
+            // Think carefully about what indexes need incrementing
             index1++;
-            index2++;
         }
     }
 
@@ -86,7 +108,8 @@ string[] Merge(string[] list1, string[] list2)
     }
     while (index2 < list2.Length)
     {
-        newlist[indexNewList++] = list2[index2++];
+        // TODO - complete this block for list2
+        index2++;
     }
 
     return newlist;
