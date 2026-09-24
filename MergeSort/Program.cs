2f7df521db2d91b637b296a1578642b4d1dd052e
ajbearon <99669470+ajbearon@users.noreply.github.com>
2026-09-24 21:23:49 +0100

    Swap functions back again

--- MergeSort/Program.cs
+++ MergeSort/Program.cs
@@ -38,6 +38,35 @@ Debug.Assert(items[5] == "Washington");
 
 Console.WriteLine("Finished.");
 
+void MergeSort(string[] items)
+{
+    // TODO - add comment to explain this code
+    var sortedLists = new List<string[]>();
+    for (int i = 0; i < items.Length; i++)
+    {
+        sortedLists.Add(new[] { items[i] });
+    }
+
+    // TODO - add comment to explain this code
+    while (sortedLists.Count > 1)
+    {
+        int index = 0;
+        while (index < sortedLists.Count - 1)
+        {
+            // TODO - add comment to explain this code
+            var sortedList = Merge(sortedLists[index], sortedLists[index + 1]);
+            sortedLists[index] = sortedList;
+            sortedLists.RemoveAt(index + 1);
+            index++;
+        }
+    }
+
+    for (int i = 0; i < items.Length; i++)
+    {
+        items[i] = sortedLists[0][i];
+    }
+}
+
 string[] Merge(string[] list1, string[] list2)
 {
     var newlist = new string[list1.Length + list2.Length];
@@ -84,34 +113,5 @@ string[] Merge(string[] list1, string[] list2)
     }
 
     return newlist;
-}
-
-void MergeSort(string[] items)
-{
-    // TODO - add comment to explain this code
-    var sortedLists = new List<string[]>();
-    for (int i = 0; i < items.Length; i++)
-    {
-        sortedLists.Add(new[] { items[i] });
-    }
-
-    // TODO - add comment to explain this code
-    while (sortedLists.Count > 1)
-    {
-        int index = 0;
-        while (index < sortedLists.Count - 1)
-        {
-            // TODO - add comment to explain this code
-            var sortedList = Merge(sortedLists[index], sortedLists[index + 1]);
-            sortedLists[index] = sortedList;
-            sortedLists.RemoveAt(index + 1);
-            index++;
-        }
-    }
-
-    for (int i = 0; i < items.Length; i++)
-    {
-        items[i] = sortedLists[0][i];
-    }
 }
 
